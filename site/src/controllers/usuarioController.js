20b844e25256eb510674ee02307fc276a4ccbfc3
gabrielCavalcanti-silva <gabriel.csilva@sptech.school>
2022-06-08 15:42:16 -0300

    Finalizado

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -65,6 +65,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var fkVotacao = req.body.fkVotacaoServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -73,10 +74,13 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else {
+    } else if (fkVotacao == undefined) {
+        res.status(400).send("Sua facção está undefined!");
+    } 
+    else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, fkVotacao)
             .then(
                 function (resultado) {
                     res.json(resultado);
